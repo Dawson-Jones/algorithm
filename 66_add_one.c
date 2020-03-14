1b9c98c44961ca41714b4a01922e46fca17ea071
dawson <957360688@qq.com>
2020-03-15 03:11:30 +0800

    refine code

--- 66_add_one.c
+++ 66_add_one.c
@@ -1,9 +1,14 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+/*
+给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+输入: [1,2,3]
+输出: [1,2,4]
+解释: 输入数组表示数字 123。
+*/
+
 #include <stdio.h>
-
-
 int reverse(int *point, int size){
     int res = 0;
     for (int i =1;i<=size;i++)
