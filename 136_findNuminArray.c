53a2b569eb51bd55cf4d522a0c19dda154edfdd2
dawson <957360688@qq.com>
2020-03-15 03:02:49 +0800

    修改了判断数独的代码, 之前的是错的

--- 136_findNuminArray.c
+++ 136_findNuminArray.c
@@ -1,9 +1,18 @@
+/**
+给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+输入: [2,2,1]
+输出: 1
+
+输入: [4,1,2,1,2]
+输出: 4
+*/
+
 #include<stdio.h>
 
 int singleNumber(int* nums, int numsSize){
     int res = 0;
-    int length = numsSize/sizeof(int);
-    for(int i=0;i<length;i++)
+    for(int i=0;i<numsSize;i++)
         res ^= nums[i];
     return res;
 }
--- 36_valid_sudoku.c
+++ 36_valid_sudoku.c
@@ -1,72 +1,105 @@
+/*
+判断一个 9x9 的数独是否有效。只需要根据以下规则，验证已经填入的数字是否有效即可。
+
+数字 1-9 在每一行只能出现一次。
+数字 1-9 在每一列只能出现一次。
+数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+
+输入:
+[
+    ["5","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]
+]
+输出: true
+*/
+
 #include <stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
 
-int isValidSudoku(char **board, int boardSize){
+bool isValidSudoku(char** board, int boardSize, int* boardColSize){
+    int *rows[9];   // |
+    int *cols[9];   // -> 一共27个指针, 都是指向不同的 int数组
+    int *box[9];    // |
+    for(int i = 0;i<3;i++){
+        for(int j = 0;j<9;j++){
+            int *temp = (int *)calloc(10, 4);
+            if(temp==NULL) exit(1);
+            switch (i){
+            case 0:
+                rows[j] = temp;
+                break;
+            case 1:
+                cols[j] = temp;
+                break;
+            case 2:
+                box[j] = temp;
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
     for (int i = 0; i < boardSize; i++){
-        int nums1[10];
-        for (int i = 0; i < 10; i++)
-            nums1[i] = 0;
-
-        int nums2[10];
-        for (int i = 0; i < 10; i++)
-            nums2[i] = 0;
-
-        for (int j = 0; i < 9; j++){
-            if (board[i][j] != '.'){
-                int current = board[i][j] - '0';
-                nums1[current]++;
-                if (nums1[current] > 1)
-                    return 0;
-            }
-            if (board[j][i] != '.'){
-                int current = board[j][i] - '0';
-                nums2[current]++;
-                if (nums2[current] > 1)
-                    return 0;
-            }
-        }
-    }
-    int current_row = 0;
-    while (current_row < 9){
-        int current_col = 0;
-        int i = 0;
-        for (i; i < current_row + 3; i++){
-            while (current_col < 9){
-                int nums[10];
-                for (int i = 0; i < 10; i++)
-                    nums[i] = 0;
-
-                int j = 0;
-                for (j; j < current_col + 3; j++){
-                    if (board[i][j] != '.'){
-                        int current = board[i][j] - '0';
-                        nums[current]++;
-                        if (nums[current] > 1)
-                            return 0;
-                    }
+        for (int j = 0; j < boardColSize[i]; j++){
+            if(board[i][j] == '.')
+                continue;
+            int cur = board[i][j] - '0';
+            rows[i][cur]++;
+            cols[j][cur]++;
+            box[i/3*3+j/3][cur]++;
+            if(rows[i][cur]>1||cols[j][cur]>1||box[i/3*3+j/3][cur]>1){
+                for(int i=0;i<9;i++){
+                    free(rows[i]);
+                    free(cols[i]);
+                    free(box[i]);
                 }
-                current_col += 3;
+                return false;
             }
         }
-        current_row += 3;
     }
-    return 1;
+    for(int i=0;i<9;i++){
+        free(rows[i]);
+        free(cols[i]);
+        free(box[i]);
+    }
+    return true;
 }
 
-int main(int argc, char const *argv[]){
-    char *string[9];
-    for (int i = 0; i < 9; i++){
-        for (int j = 0; j < 9; j++){
-            *string[i] = j;
-            string[i]++;
-            j++;
-        }
-    }
 
-    int i = isValidSudoku(string, 9);
-    if (i == 0){
-        printf("false");
-        return 0;
-    }
-    printf("true");
+int main(int argc, char const *argv[]){
+    char *board[9];
+    char a[] = {'5','3','.','.','7','.','.','.','.'};
+    char b[] = {'6','.','.','1','9','5','.','.','.'};
+    char c[] = {'.','9','8','.','.','.','.','6','.'};
+    char d[] = {'8','.','.','.','6','.','.','.','3'};
+    char e[] = {'4','.','.','8','.','3','.','.','1'};
+    char f[] = {'7','.','.','.','2','.','.','.','6'};
+    char g[] = {'.','6','.','.','.','.','2','8','.'};
+    char h[] = {'.','.','.','4','1','9','.','.','5'};
+    char i[] = {'.','.','.','.','8','.','.','7','9'};
+    board[0] = a;
+    board[1] = b;
+    board[2] = c;
+    board[3] = d;
+    board[4] = e;
+    board[5] = f;
+    board[6] = g;
+    board[7] = h;
+    board[8] = i;
+
+    int boardSize[9];
+    for(int i = 0;i<9;i++)
+        boardSize[i] = 9;
+    int res = isValidSudoku(board, 9, boardSize);
+    res==0?printf("false\n"):printf("true\n");
     return 0;
 }
