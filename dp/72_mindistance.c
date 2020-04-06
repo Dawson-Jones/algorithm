00cb2278d44a201512326588721e73ac9e03fa2c
dawson jones <957360688@qq.com>
2020-04-06 23:33:57 +0800

    refine 72 code

--- dp/72_mindistance.c
+++ dp/72_mindistance.c
@@ -28,15 +28,14 @@ int minDistance(char *word1, char *word2) {
     int n1 = strlen(word1);
     int n2 = strlen(word2);
     int dp[n1 + 1][n2 + 1];
-    dp[0][0] = 0;
 
     // 第一行
-    for (int j = 1; j < n2 + 1; ++j) {
-        dp[0][j] = dp[0][j - 1] + 1;
+    for (int j = 0; j < n2 + 1; ++j) {
+        dp[0][j] = j;
     }
     // 第一列
-    for (int i = 1; i < n1 + 1; ++i) {
-        dp[i][0] = dp[i - 1][0] + 1;
+    for (int i = 0; i < n1 + 1; ++i) {
+        dp[i][0] = i;
     }
     for (int i = 1; i < n1 + 1; ++i) {
         for (int j = 1; j < n2 + 1; ++j) {
